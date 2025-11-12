249396f6be33d37b77d87f022e6ee9fffd7bd59c
leidy alvarez <leidy.alvarez0429@gmail.com>
2025-11-11 20:14:04 -0500

    Initial configuration of the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CityModule } from './city/city.module';
+import { RulesModule } from './rules/rules.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CityModule, RulesModule, VehiclesModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })

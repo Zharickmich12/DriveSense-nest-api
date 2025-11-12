ede8c64660ed96708d765f9b17c270fee3afb039
MaripVargas <maripvargas0627@gmail.com>
2025-11-12 18:42:41 -0500

    feat(users): implement UsersService CRUD (incl. password hashing)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private usersRepo: Repository<User>,
+  ) {}
+
+  async create(dto: CreateUserDto): Promise<User> {
+    const hashed = await bcrypt.hash(dto.password, 10);
+    const user = this.usersRepo.create({ ...dto, password: hashed });
+    return this.usersRepo.save(user);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  findAll(limit = 10, page = 1) {
+    return this.usersRepo.find({
+      take: limit,
+      skip: (page - 1) * limit,
+      order: { id: 'ASC' },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const u = await this.usersRepo.findOneBy({ id });
+    if (!u) throw new NotFoundException('User not found');
+    return u;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, dto: UpdateUserDto) {
+    const user = await this.findOne(id);
+    if (dto.password) {
+      user.password = await bcrypt.hash(dto.password, 10);
+    }
+    Object.assign(user, dto);
+    return this.usersRepo.save(user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    return this.usersRepo.remove(user);
+  }
+
+  async findByEmail(email: string) {
+    return this.usersRepo.findOneBy({ email });
   }
 }

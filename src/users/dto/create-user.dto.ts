4fb32461d8bbf63b72ed003bcbf23a3c83f9ebf1
MaripVargas <maripvargas0627@gmail.com>
2025-11-12 18:40:12 -0500

    [FEAT] add CreateUserDTO and UpdateUserDTO with validations

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, MinLength, IsOptional, IsIn } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsIn(['admin','user'])
+  role?: 'admin' | 'user';
+}

78c29ef898c56a2c2e4ef18b9d8579ba4827660d
EileenMendoza18 <eileenjml4816@gmail.com>
2025-11-12 20:47:39 -0500

    Create entity for the cities module

--- src/city/entities/city.entity.ts
+++ src/city/entities/city.entity.ts
@@ -1,1 +1,23 @@
-export class City {}
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn,  UpdateDateColumn} from "typeorm";
+
+@Entity('cities')
+export class City {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    name: string;
+
+    @Column({ default: false })
+    isActive: boolean; // Si tiene pico y placa activo
+
+    @Column({ nullable: true })
+    description: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}

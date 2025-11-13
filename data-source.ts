4c528f859fe6737258de33153d4f5904f5123d91
MaripVargas <maripvargas0627@gmail.com>
2025-11-12 19:38:38 -0500

    [FIX] Bug fixes in configurations

--- data-source.ts
+++ data-source.ts
@@ -9,9 +9,9 @@ export default new DataSource({
   port: parseInt(process.env.DB_PORT || '3306', 10),
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'DriveSenseDB',
-  entities: ['src//*.entity{.ts,.js}'],
-  migrations: ['src/migrations/*{.ts,.js}'],
+  database: process.env.DB_NAME || 'drivesensedb', // usa el mismo nombre que en DBeaver (en minúsculas)
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'], 
   synchronize: false,
   logging: true,
 });

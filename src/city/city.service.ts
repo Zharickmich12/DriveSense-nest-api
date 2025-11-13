45b3ec1f3cb9854ac6ebeddb016453bacd82b391
EileenMendoza18 <eileenjml4816@gmail.com>
2025-11-12 20:49:46 -0500

    CRUD for the city module

--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,26 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCityDto } from './dto/create-city.dto';
 import { UpdateCityDto } from './dto/update-city.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { City } from './entities/city.entity';
 
 @Injectable()
 export class CityService {
-  create(createCityDto: CreateCityDto) {
-    return 'This action adds a new city';
+
+    constructor(
+    @InjectRepository(City)
+    private readonly cityRepository: Repository<City>,
+  ) {}
+
+  async create(createCityDto: CreateCityDto) {
+    const city = this.cityRepository.create(createCityDto);
+    await this.cityRepository.save(city);
+    return {
+      message: 'City created successfully.',
+      data: city,
+    };
   }
 
-  findAll() {
-    return `This action returns all city`;
+  async findAll() {
+    const cities = await this.cityRepository.find();
+    return {
+      message: `List of registered cities (${cities.length}).`,
+      data: cities,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} city`;
+  async findOne(id: number) {
+    const city = await this.cityRepository.findOneBy({ id });
+    if (!city) throw new NotFoundException(`The city with id ${id} was not found.`);
+    return city;
   }
 
-  update(id: number, updateCityDto: UpdateCityDto) {
-    return `This action updates a #${id} city`;
+  async update(id: number, updateCityDto: UpdateCityDto) {
+    const city = await this.cityRepository.preload({
+      id,
+      ...updateCityDto,
+    });
+    if (!city) throw new NotFoundException(`The city with id ${id} was not found.`);
+
+    await this.cityRepository.save(city);
+    return {
+      message: 'City updated successfully.',
+      data: city,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} city`;
+  async remove(id: number) {
+    const city = await this.findOne(id);
+    await this.cityRepository.remove(city);
+    return { message: 'City deleted successfully.' };
   }
+
 }
+  
+

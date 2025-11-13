edf1ce8b07c0484c427ef084e21434498d932c9e
EileenMendoza18 <eileenjml4816@gmail.com>
2025-11-12 20:52:11 -0500

    CRUD for the rules module

--- src/rules/rules.module.ts
+++ src/rules/rules.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RulesService } from './rules.service';
 import { RulesController } from './rules.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rule} from './entities/rule.entity';
+import { City } from 'src/city/entities/city.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Rule, City])],
   controllers: [RulesController],
   providers: [RulesService],
 })
--- src/rules/rules.service.ts
+++ src/rules/rules.service.ts
@@ -1,26 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRuleDto } from './dto/create-rule.dto';
 import { UpdateRuleDto } from './dto/update-rule.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Rule } from './entities/rule.entity';
+import { City } from 'src/city/entities/city.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RulesService {
-  create(createRuleDto: CreateRuleDto) {
-    return 'This action adds a new rule';
+
+   constructor(
+    @InjectRepository(Rule)
+    private readonly ruleRepository: Repository<Rule>,
+    @InjectRepository(City)
+    private readonly cityRepository: Repository<City>,
+  ) {}
+
+  async create(createRuleDto: CreateRuleDto) {
+    const { cityId, ...data } = createRuleDto;
+    const city = await this.cityRepository.findOneBy({ id: cityId });
+
+    if (!city) throw new NotFoundException(`The city with id ${cityId} was not found. `);
+
+    const rule = this.ruleRepository.create({ ...data, city });
+    await this.ruleRepository.save(rule);
+
+    return {
+      message: 'Rule created successfully.',
+      data: rule,
+    };
   }
 
-  findAll() {
-    return `This action returns all rules`;
+  async findAll() {
+    const rules = await this.ruleRepository.find({ relations: ['city'] });
+    return {
+      message: `Total registered rules: ${rules.length}`,
+      data: rules,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} rule`;
+  async findOne(id: number) {
+    const rule = await this.ruleRepository.findOne({
+      where: { id },
+      relations: ['city'],
+    });
+    if (!rule) throw new NotFoundException(`The rule with id ${id} was not found.`);
+    return rule;
   }
 
-  update(id: number, updateRuleDto: UpdateRuleDto) {
-    return `This action updates a #${id} rule`;
+  async update(id: number, updateRuleDto: UpdateRuleDto) {
+    const rule = await this.ruleRepository.findOne({ where: { id } });
+    if (!rule) throw new NotFoundException(`The rule with id ${id} was not found.`);
+
+    Object.assign(rule, updateRuleDto);
+    await this.ruleRepository.save(rule);
+
+    return {
+      message: 'Rule updated successfully.',
+      data: rule,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} rule`;
+  async remove(id: number) {
+    const rule = await this.findOne(id);
+    await this.ruleRepository.remove(rule);
+    return { message: 'Rule deleted successfully.' };
   }
+
 }

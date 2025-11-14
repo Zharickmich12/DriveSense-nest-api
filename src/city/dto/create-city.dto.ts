d13bbaad02ffc9e6355c8d06178cd1661811a053
Kelly Natalia <113135035+KellyNatalia@users.noreply.github.com>
2025-11-13 20:15:57 -0500

    Add pipes and validations

--- src/city/dto/create-city.dto.ts
+++ src/city/dto/create-city.dto.ts
@@ -1,9 +1,11 @@
 import { MinLength, IsString, IsOptional, IsBoolean} from "class-validator";
+import { Transform } from "class-transformer";
 
 export class CreateCityDto {
 
     @IsString()
     @MinLength(3, { message: 'The name must have at least 3 characters.' })
+    @Transform(({ value }) => value?.charAt(0).toUpperCase() + value?.slice(1).toLowerCase())
     name: string;
 
     @IsOptional()

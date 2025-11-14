8d82aea72121c89d443fbdd7403e87fee76f022a
Zharick Michelle <128750315+Zharickmich12@users.noreply.github.com>
2025-11-13 19:27:44 -0500

    implement VehiclesService with CRUD operations

--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -1,26 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ForbiddenException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Vehicle } from './entities/vehicle.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class VehiclesService {
-  create(createVehicleDto: CreateVehicleDto) {
-    return 'This action adds a new vehicle';
+  constructor(
+    @InjectRepository(Vehicle)
+    private vehicleRepo: Repository<Vehicle>
+  ) {}
+
+  async create(data: CreateVehicleDto, user: User) {
+    const vehicle = this.vehicleRepo.create({ ...data, user });
+    return await this.vehicleRepo.save(vehicle);
   }
 
-  findAll() {
-    return `This action returns all vehicles`;
+  async findAll(user: User) {
+    return await this.vehicleRepo.find({ where: { user: { id: user.id } } });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} vehicle`;
+  async findOne(id: number, user: User) {
+    const vehicle = await this.vehicleRepo.findOne({ where: { id, user: { id: user.id } } });
+    if (!vehicle) throw new NotFoundException('Vehicle not found');
+    return vehicle;
   }
 
-  update(id: number, updateVehicleDto: UpdateVehicleDto) {
-    return `This action updates a #${id} vehicle`;
+  async update(id: number, data: UpdateVehicleDto, user: User) {
+    const vehicle = await this.findOne(id, user);
+    Object.assign(vehicle, data);
+    return await this.vehicleRepo.save(vehicle);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} vehicle`;
+  async remove(id: number, user: User) {
+    const vehicle = await this.findOne(id, user);
+    return await this.vehicleRepo.remove(vehicle);
   }
 }

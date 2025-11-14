07a5e353c2da1febbf98427bf9e7c71f68f4ea5a
Zharick Michelle <128750315+Zharickmich12@users.noreply.github.com>
2025-11-13 19:14:24 -0500

    add Vehicle entity with user relation

--- src/vehicles/entities/vehicle.entity.ts
+++ src/vehicles/entities/vehicle.entity.ts
@@ -1,1 +1,34 @@
-export class Vehicle {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+
+export type VehicleType = 'car' | 'motorcycle';
+
+@Entity('vehicles')
+export class Vehicle {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 20, unique: true })
+  licensePlate: string;
+
+  @Column({ length: 50 })
+  brand: string;
+
+  @Column({ length: 50 })
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column({ type: 'varchar', default: 'car' })
+  type: VehicleType;
+
+  @ManyToOne(() => User, user => user.id, { eager: true })
+  user: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}

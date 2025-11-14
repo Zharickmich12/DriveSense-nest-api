93b8162a8aa65be70b09b10deb4db33b7acdd8ea
Kelly Natalia <kellynatalia_toro16@utp.edu.co>
2025-11-13 20:49:19 -0500

    Swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/global-exception.filter';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Filtro global para excepciones
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  // Cargar el archivo YAML
+  const swaggerDocument = YAML.load(join(__dirname, '..', 'docs', 'api.yaml'));
+
+  // Servir Swagger UI en un endpoint
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   // Pipe global para validaciones
   app.useGlobalPipes(

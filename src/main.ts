27014490e925294b38b5348416f893764451cf1c
Kelly Natalia <kellynatalia_toro16@utp.edu.co>
2025-11-13 20:14:18 -0500

    Add pipes and valitations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Filtro global para excepciones
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  // Pipe global para validaciones
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`App running on: http://localhost:${port}`);
 }
+
 bootstrap();

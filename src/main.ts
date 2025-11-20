893e732fd9c4a3a4fe71bf63e582fbc6d0b24764
Zharick Michelle <128750315+Zharickmich12@users.noreply.github.com>
2025-11-19 20:50:21 -0500

    swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import * as YAML from 'yamljs';
 import { join } from 'path';
 import { LogsService } from './logss/logs.service';
 import { LoggingInterceptor } from './common/interceptors/login.interceptor';
+import { SwaggerCustomOptions } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -28,6 +29,19 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+   const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      url: '/api-json'
+    },
+    customCssUrl: [
+      'https://unpkg.com/swagger-ui-dist@5.10.3/swagger-ui.css',
+    ],
+    customJs: [
+      'https://unpkg.com/swagger-ui-dist@5.10.3/swagger-ui-bundle.js',
+      'https://unpkg.com/swagger-ui-dist@5.10.3/swagger-ui-standalone-preset.js',
+    ]
+  }
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
